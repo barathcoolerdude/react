85432090966517632b69e456dc8175d926fcc7ef
coolerdude <barathelango3@gmail.com>
2024-10-25 10:43:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function MainContent(){
+    return (
+        <h1>In learning React!</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <MainContent/>
+    </div>,
+    document.getElementById("root"));
