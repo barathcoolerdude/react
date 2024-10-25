77a5f192442ef280f1c63d5a04f2f22bebb4e9c7
coolerdude <barathelango3@gmail.com>
2024-10-25 20:20:35 +0530

    second

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function MainContent(){
-    return (
-        <h1>In learning React!</h1>
-    )
-}
+// const element = <h1 classsName="header">this is jsx</h1>
+// console.log(element);
 
-ReactDOM.render(
-    <div>
-        <MainContent/>
-    </div>,
-    document.getElementById("root"));
+// ReactDOM.render(
+//     <div>
+//     element
+//     <p>this is a paragraph</p>
+//     </div>
+//     , document.getElementById("root"));
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "this is an imperative way to program";
+// h1.className = "header";
+// document.getElementById("root").append(h1)
+
+const navbar = (
+    <nav>
+        <h1>Bob's bistro</h1>
+        <ul>
+            <li>menu</li>       
+            <li>history</li>       
+            <li>list</li>       
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar , document.getElementById("root"))
