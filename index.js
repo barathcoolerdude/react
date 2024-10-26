0029a03a0ffddd7b8e5907ef305baf45425b9d99
coolerdude <barathelango3@gmail.com>
2024-10-26 11:03:11 +0530

    react things

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-// const element = <h1 classsName="header">this is jsx</h1>
-// console.log(element);
-
-// ReactDOM.render(
-//     <div>
-//     element
-//     <p>this is a paragraph</p>
-//     </div>
-//     , document.getElementById("root"));
-
-// const h1 = document.createElement("h1");
-// h1.textContent = "this is an imperative way to program";
-// h1.className = "header";
-// document.getElementById("root").append(h1)
-
-const navbar = (
-    <nav>
-        <h1>Bob's bistro</h1>
-        <ul>
-            <li>menu</li>       
-            <li>history</li>       
-            <li>list</li>       
-        </ul>
-    </nav>
+const webpage = (
+    <div>
+    <img src="./images/image.jpeg" alt="image" />
+    <h1>this is react</h1>
+    <ul>
+        <li>thing1</li>
+        <li>thing2</li>
+        <li>thing3</li>
+        <li>thing4</li>
+    </ul>
+    </div>
 )
 
-ReactDOM.render(navbar , document.getElementById("root"))
+ReactDOM.render(webpage , document.getElementById("root"));
